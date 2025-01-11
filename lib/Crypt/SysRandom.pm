b662655a8e8a8ac74b96bd6513fec9879e45cc01
Leon Timmermans <fawaka@gmail.com>
2025-01-12 00:44:15 +0100

    Initial implementation

--- lib/Crypt/SysRandom.pm
+++ lib/Crypt/SysRandom.pm
@@ -7,4 +7,22 @@ use XSLoader;
 
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
+use Exporter 'import';
+our @EXPORT_OK = 'random_bytes';
+
 1;
+
+# ABSTRACT: Perl interface to system randomness
+
+=head1 SYNOPSIS
+
+ use Crypt::SysRandom 'random_bytes';
+ my $random = random_bytes(16);
+
+=head1 DESCRIPTION
+
+This module uses whatever C interface is available to procure cryptographically random data from the system.
+
+=func random_bytes($count)
+
+This will fetch a string of C<$count> random bytes containing cryptographically secure random date.
