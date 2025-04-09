a809d1324bae2a9d71275ed33557db57c697f0fc
Leon Timmermans <fawaka@gmail.com>
2025-04-09 17:51:32 +0200

    Add arc4random support

--- planner/xs.pl
+++ planner/xs.pl
@@ -32,6 +32,26 @@ int main(void)
         return 0;
 }
 EOF
+	['arc4random in sys/random.h', 'SYS_RANDOM_ARC4RANDOM', {}, <<EOF ],
+#include <sys/random.h>
+
+int main(void)
+{
+        char buf[16];
+        arc4random_buf(buf, sizeof(buf));
+        return 0;
+}
+EOF
+	['arc4random in unistd.h', 'UNISTD_ARC4RANDOM', {}, <<EOF ],
+#include <unistd.h>
+
+int main(void)
+{
+        char buf[16];
+        arc4random_buf(buf, sizeof(buf));
+        return 0;
+}
+EOF
 	['Microsoft BcryptGenRandom', 'BCRYPT_GENRANDOM', { libraries => ['Bcrypt'] }, <<EOF ],
 #define WIN32_NO_STATUS
 #include <windows.h>
